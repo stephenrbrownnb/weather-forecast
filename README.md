8696691c7f27a2e425df197e5ae728f2f0d7d608
Stephen Brown <stephenrbrownnb@gmail.com>
2023-03-30 11:24:08 -0300

    Updated readme and javascript

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# weather-forecast
+Challange 10 Weather Forecast
+Contents
+Dexscription Installation Usage Instructions Credit License
+
+Description
+A website which gives the 5 day weather forecast
+
+Installation
+As this is a website there is no installation required.
+Usage Instructions
+Go to the website at https://stephenrbrownnb.github.io/weather-forecast/ 
+
+Credit
+This project was created by Stephen Brown , Github id: stephenrbrownnb and email address: stephenrbrownnb@gmail.com
+
+License
+Copyright (c) [2023] [Stephen]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,100 +1,82 @@
-var userFormEl = document.querySelector('#user-form');
-var languageButtonsEl = document.querySelector('#language-buttons');
-var nameInputEl = document.querySelector('#username');
-var repoContainerEl = document.querySelector('#repos-container');
-var repoSearchTerm = document.querySelector('#repo-search-term');
+const form = document.querySelector('#user-form');
+const searchHistory = document.querySelector('#search-history');
+const searchTerm = document.querySelector('#repo-search-term');
+const reposContainer = document.querySelector('#repos-container');
 
-var formSubmitHandler = function (event) {
+let cities = [];
+
+// Add event listener to form
+form.addEventListener('submit', function(event) {
   event.preventDefault();
-
-  var username = nameInputEl.value.trim();
-
-  if (username) {
-    getUserRepos(username);
-
-    repoContainerEl.textContent = '';
-    nameInputEl.value = '';
-  } else {
-    alert('Please enter a GitHub username');
+  const input = document.querySelector('#username');
+  const city = input.value.trim();
+  if (city.length) {
+    geocode(city);
+    input.value = '';
   }
-};
+});
 
-var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
-
-  if (language) {
-    getFeaturedRepos(language);
-
-    repoContainerEl.textContent = '';
+// Add event listener to search history
+searchHistory.addEventListener('click', function(event) {
+  if (event.target.matches('button')) {
+    const city = event.target.textContent.trim();
+    geocode(city);
   }
-};
+});
 
-var getUserRepos = function (user) {
-  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
-
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data, user);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to GitHub');
-    });
-};
-
-var getFeaturedRepos = function (language) {
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
+// Geocode city name to get latitude and longitude
+async function geocode(city) {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(city)}.json?access_token={2943a7908b1bb114d559f8b52228f1f7}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const { center } = data.features[0];
+    const [longitude, latitude] = center;
+    getWeather(latitude, longitude, city);
+  } catch (error) {
+    console.error(error);
+    alert('An error occurred while geocoding the city.');
   }
+}
 
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
+// Get weather data from API
+async function getWeather(latitude, longitude, city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={2943a7908b1bb114d559f8b52228f1f7}&units=metric`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayWeather(data, city);
+  } catch (error) {
+    console.error(error);
+    alert('An error occurred while fetching weather data.');
   }
-};
+}
 
-userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+// Display weather data in UI
+function displayWeather(data, city) {
+  // Update search term
+  searchTerm.textContent = city;
+
+  // Create weather card
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `
+    <div class="card-body">
+      <h3 class="card-title">${city}</h3>
+      <p class="card-text">${data.weather[0].description}</p>
+      <p class="card-text">${data.main.temp} &#8451;</p>
+    </div>
+  `;
+
+  // Add card to container
+  reposContainer.appendChild(card);
+
+  // Add city to search history
+  if (!cities.includes(city)) {
+    cities.push(city);
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-block');
+    button.textContent = city;
+    searchHistory.appendChild(button);
+  }
+}
